4f9bc6949649027a0e9ea98143db989d8d2a67c0
caiobmdev <caiocbm1999@gmail.com>
2025-08-08 14:19:31 -0300

    Opção de saida
    incluso no sistema a opção de saída para o usuário

--- Projeto Cartorio Ebac.c
+++ Projeto Cartorio Ebac.c
@@ -123,6 +123,7 @@ int main()
 	printf("\t 1-Registrar nomes\n");
 	printf("\t 2-consultar nomes\n");
 	printf("\t 3-deletar nomes\n\n");
+	printf("\t 4-sair do sistema\n\n");
 	printf("Opção desejada: ");   //fim do menu
 	
 	scanf("%d", &opcao);  //armazenando a escolha do usuario
@@ -141,6 +142,11 @@ int main()
 		
 		case 3:	
 		deletar();
+		break;
+		
+		case 4:
+		printf("obrigado por utilizar o sistema!\n");
+		return 0;
 		break;
 		
 		default:
